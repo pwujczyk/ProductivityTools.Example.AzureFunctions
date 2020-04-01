a0445c197ce19c7f195f51da2766911fa1c3cd4d
pwujczyk <pwujczyk@ecovadis.com>
2020-04-01 16:08:09 +0200

    upgrade of packages and put to quene

--- ProductivityTools.Example.AzureFunctions/OnPaymentReceived.cs
+++ ProductivityTools.Example.AzureFunctions/OnPaymentReceived.cs
@@ -1,10 +1,10 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
@@ -15,21 +15,19 @@ namespace ProductivityTools.Example.AzureFunctions
         [FunctionName("OnPaymentReceived")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            [Queue("orders")]IAsyncCollector<Order> orderQuene,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var order= JsonConvert.DeserializeObject<Order>(requestBody);
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            await orderQuene.AddAsync(order);
 
-            return new OkObjectResult(responseMessage);
+            log.LogInformation($"Order {order.Name} received");
+
+            return new OkObjectResult("Thank you for the order");
         }
     }
 }
---
+++ ProductivityTools.Example.AzureFunctions/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.Example.AzureFunctions
+{
+    public class Order
+    {
+        public int Value { get; set; }
+        public string Name { get; set; }
+    }
+}
